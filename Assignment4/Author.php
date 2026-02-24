061d9d0e28d525a3eb07089a1770bbd552cd2585
Manarbek <81147013+manarbek-coder@users.noreply.github.com>
2026-02-24 13:55:24 +0500

    Add files via upload

---
+++ Assignment4/Author.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    /**
+     * Get the books for the author.
+     */
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+}
